d10c3ba376f14b55aab6c8976d4d14a3e6558862
Jason Eliasen <eliasenjason@gmail.com>
2021-05-17 16:49:39 -0600

    frontend getting data now from backend

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -2,10 +2,10 @@ import Rating from "./Rating"
 
 
 export default function Product({product}) {
-    return (
+    return ( 
         <div key={product._id} className="card">
             <a href={`/product/${product._id}`}>
-                <img className="medium" src={`../${product.image}`} alt={product.name} />
+                <img className="medium" src={`..${product.image}`} alt={product.name} />
             </a>
             <div className="card-body">
                 <a href={`/product/${product._id}`}>
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,40 @@
-import data from '../data'
 import Product from '../components/Product'
+import LoadingBox from '../components/LoadingBox'
+import MessageBox from '../components/MessageBox'
+import {useState, useEffect} from 'react'
 
 export default function HomeScreen() {
+    const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(false)
+
+    useEffect(() => {
+            setLoading(true)
+            fetch('/api/products')
+            .then(res => {
+                if(res.status !== 200) {
+                    setError(res)
+                }
+                return res.json()
+            })
+            .then(data => setProducts(data))
+            .then(() => setLoading(false))
+    },[])
+
     return (
-        <div className="row center">
-        {
-        data.products.map(product => (
-        <Product key={product._id} product={product}></Product>
-        ))
-        }
+        <div>
+            {error ? <MessageBox variant="danger">{`Request Failed - ${error.statusText} / Status code ${error.status}`}</MessageBox>
+            :
+            loading ? <LoadingBox></LoadingBox>
+            :
+            <div className="row center">
+                {products.map(product => (
+                    <Product key={product._id} product={product}></Product>
+                ))}
+            </div>
+            }
+            
         </div>
-    )
+        
+        )
 }
